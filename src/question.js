ece266d9fbe1d49bc6d793b1bf4bf7fddd40075e
Boyoung Yun <index_1234@naver.com>
2022-08-07 22:37:50 +0900

    Add status bar in question.js

--- src/question.js
+++ src/question.js
@@ -1,12 +1,15 @@
 import React, {useState} from 'react';
 import qnaList from './qnaList';
 import { CSSTransition, SwitchTransition, TransitionGroup } from "react-transition-group";
+import beer1 from './img/beer_1.png'
 
 function Question() {
     let [cnt, setCnt] = useState(0);
     let [choice, setChoice] = useState(['E','I','N','S','T','F','J','P']);
     let [point, setPoint] = useState([0,0,0,0,0,0,0,0]);
     let [mbti, setMbti] = useState('');
+    let endPoint = 11;
+    let status = (100/endPoint)*cnt;
     return (
         <div className="outer mx-auto">
             <div className="container">
@@ -16,12 +19,25 @@ function Question() {
                         {
                             cnt>11
                             ? <div className="container">
-                                <div>{mbti}</div>
+                                
+                                    <h1 className="headline mb-5">당신과 어울리는 술은...</h1>
+                                    <div className="main mx-auto pb-5">
+                                    <div className="col-12 mx-auto px-3 py-4">
+                                        <img className="mx-auto" style={{width:'30%'}} src={beer1}></img>
+                                    </div>
+                                    <span>ENFP<br/></span>
+                                    <span>생맥주</span>
+                                </div>
                             </div>
                             :
                             <div className="container">
                                 <h1 className="headline">Q{cnt+1}.</h1>
+                                <div className="status mx-auto">
+                                    <div className="statusBar" style={{width:status+'%'}}></div>
+                                </div>
+                                <div className="qnaHeadline mb-5 mt-2" style={{fontSize:'15px', color:'#cd3b3b'}}>{cnt+1}/12</div>
                                 <h1 className="qnaHeadline mb-5">{qnaList[cnt].q}</h1>
+                                
                                 <div className="qnaBox px-2 py-2 mx-auto mb-3" onClick={()=>{setCnt(cnt+1); Select(qnaList[cnt].m_a);}}><span>A. {qnaList[cnt].a}<br/></span></div>
                                 <div className="qnaBox px-2 py-2 mx-auto" onClick={()=>{setCnt(cnt+1); Select(qnaList[cnt].m_b);}}><span>B. {qnaList[cnt].b}</span></div>
                             </div> 
